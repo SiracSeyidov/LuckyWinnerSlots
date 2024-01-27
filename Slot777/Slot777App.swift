88769de9e2dfcc8dc6d12edd418fccdb5e85123d
SeyidovSirac <60558048+SiracSeyidov@users.noreply.github.com>
2024-01-27 22:37:53 +0400

    Initial Commit

---
+++ Slot777/Slot777App.swift
@@ -0,0 +1,17 @@
+//
+//  Slot777App.swift
+//  Slot777
+//
+//  Created by Sirac Seyidov on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Slot777App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
